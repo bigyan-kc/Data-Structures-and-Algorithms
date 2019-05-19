27f1e92cb68028412db00fc97e757a0eaef59e81
Bigyan <b.kc@spartans.nsu.edu>
2019-05-19 11:14:15 -0400

    Data Structures added. Not unit tested.

--- src/edu/nsu/Main.java
+++ src/edu/nsu/Main.java
@@ -1,16 +1,18 @@
 package edu.nsu;
 
-import edu.nsu.Algorithms.MergeSort;
-import edu.nsu.Algorithms.Sorting;
+
+import edu.nsu.LinkedListImpl;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] myNumbers = {10,-17, -19, -20, -30, 0, 5};
-        Sorting sorting = new MergeSort();
-        sorting.sortArray(myNumbers, 0, myNumbers.length-1);
-        for(int num: myNumbers)
-            System.out.println(num);
+        LinkedListImpl linkedList = new LinkedListImpl();
+        linkedList.insertNode(12);
+        linkedList.insertNode(24);
+        linkedList.insertNode(44);
+        linkedList.insertNode(-1);
+        linkedList.displayLinkedList();
+
     }
 }
